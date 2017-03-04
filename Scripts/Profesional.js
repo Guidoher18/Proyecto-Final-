8094e8cf1402419fd9c228135d28e84484937d64
Guidoher18 <guido_181193@outlook.com>
2017-03-04 00:56:07 -0300

    Probando jQuery con Título y Especialidad

--- Scripts/Profesional.js
+++ Scripts/Profesional.js
@@ -1,3 +1,13 @@
 /*AGREGAR FUNCIÓN PARA QUE CUANDO HAGA CLIC EN DR. o MG. se AGREGUE ESP. EN .... */
 //VALIDAR CAMPOS DEL FORMULARIO
 //FIRMA DIGITAL, por favor suba una imagen que ... etc
+// FOTO 4x4
+
+$(document).ready(function(){
+    $('#Titulo option[value="Mg."]').on(click, function(){
+        $('#InsertarEspecialista').append('<br><div id="Especialista"><label>Especialista en: </label><input type="text" name="Esp." class="form-control col-md-6"/></div><br>');
+    });
+});
+
+
+
