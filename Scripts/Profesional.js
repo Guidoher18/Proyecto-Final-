b210b840450f9e5e8e0e358580c4cab15d1ce13b
Guidoher18 <guido_181193@outlook.com>
2017-03-04 04:03:49 -0300

    Titulo-Especialidad jQuery Listo

--- Scripts/Profesional.js
+++ Scripts/Profesional.js
@@ -5,10 +5,19 @@
 //Campos required
 
 $(document).ready(function(){
-    if ($('#Mg').val("selected")){
-        alert("Hola");
-    };
+    $('#Titulo').change(function(){
+        var $elegido = $("#Titulo").val();
+        if ($elegido == "Mg."){
+            $('#InsertarEspecialista').css('display','inherit');
+            $('#Profdiv1').css({'top':'7.41302vh', 'bottom':'7.41302vh'});
+        }
+        else if ($elegido == "Dr."){
+            $('#InsertarEspecialista').css('display','inherit');
+            $('#Profdiv1').css({'top':'7.41302vh', 'bottom':'7.41302vh'});            
+        }
+        else{
+            $('#InsertarEspecialista').css('display','none');
+            $('#Profdiv1').css({'top':'12.3298vh', 'bottom':'12.3298vh'});                        
+        };        
+    });
 });
-
-
-
