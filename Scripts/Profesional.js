d1dbcf4d8250fc121ec5238c7e0dd08243c75c0d
Guidoher18 <guido_181193@outlook.com>
2017-03-04 02:54:50 -0300

    Prueba aun fallida con <select> y jQuery

--- Scripts/Profesional.js
+++ Scripts/Profesional.js
@@ -2,11 +2,12 @@
 //VALIDAR CAMPOS DEL FORMULARIO
 //FIRMA DIGITAL, por favor suba una imagen que ... etc
 // FOTO 4x4
+//Campos required
 
 $(document).ready(function(){
-    $('#Titulo option[value="Mg."]').on(click, function(){
-        $('#InsertarEspecialista').append('<br><div id="Especialista"><label>Especialista en: </label><input type="text" name="Esp." class="form-control col-md-6"/></div><br>');
-    });
+    if ($('#Mg').val("selected")){
+        alert("Hola");
+    };
 });
 
 
