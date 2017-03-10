48cae750ac6c53e8aaeed379250a790425d6a9e3
Guidoher18 <guido_181193@outlook.com>
2017-03-10 19:40:16 -0300

    Login Ordenado y Documentado =)

--- Scripts/Login.js
+++ Scripts/Login.js
@@ -1,13 +1,16 @@
-//Cuando se hace clic en #Siguiente aparece para cargar la #Contraseña
 $(document).ready(function(){
     $('button').on('click',function(){
         event.preventDefault();
     });
+
+//Cuando se hace clic en #Siguiente aparece para cargar la #Contraseña, Permanecer Conectado y Recuperar la Contraseña
     $('#Siguiente').click(function(){
         $(this).toggle();
         $('#Usuario').toggle();
         $('h2').html("Ingresar Contraseña");
         $('#Contraseña').fadeIn('slow');
         $('#Submit').toggle();
+        $('p').remove();
+        $('a').html("<a href='&'> Recuperar Contraseña </a>");        
     });
 });
