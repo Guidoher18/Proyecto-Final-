94505ff424fd1b7bce4eff16cda8abc79e692915
Guidoher18 <guido_181193@outlook.com>
2017-03-19 00:28:12 -0300

    Crear Historia, relativamente terminado, comienzo de Ver Historia

--- Scripts/CrearHistoria.js
+++ Scripts/CrearHistoria.js
@@ -1,10 +1,10 @@
+//Evita que cuando se hace clic en #AgregarVinculo se recargue por Default la página
 document.getElementById("AgregarVinculo").addEventListener("click", function(event){
     event.preventDefault()
 });
 
 
 $(document).ready(function(){
-    
 //Cuando se posiciona el cursor sobre #FotoPerfil cambia a #DragFotoPerfil 
     $('#FotoPerfil').mouseenter(function(){
         $('#DragFotoPerfil').fadeToggle(900);
@@ -16,9 +16,9 @@ $(document).ready(function(){
         $('#InputFoto').click();
     });
 
-//CalcularEdad() obtiene la diferencia entre la fecha actual y la ingresada. Toma como parámetro el selector entrecomillado y sin $() que refiere al input type=date, por ej.: '#fecha'
-    var CalcularEdad = function (selector){
-        var $FechaElegida = $(selector).val(); //devuelve la fecha elegida como yyyy-mm-dd
+//CalcularEdad() obtiene la diferencia entre la fecha actual y la ingresada. Toma como parámetro los selectores entrecomillados y sin $() que refiere al input type=date, y a su Label correspondiente por ej.: '#fecha', '#fechaLabel'.
+    var CalcularEdad = function (inputDate, inputDateLabel){
+        var $FechaElegida = $(inputDate).val(); //devuelve la fecha elegida como yyyy-mm-dd
         
         var $yyyy= moment().get('year');
         var $mm = moment().get('month');
@@ -42,61 +42,59 @@ $(document).ready(function(){
             $PalabraMes = 'Meses'
         }
 
-        if (selector == '#FechaNacimiento input[type="date"]')
+        if (inputDate == '#FechaNacimiento input[type="date"]')
         {
             if ($Edad < 18 && $DifMeses != 0) //Si es un menor de 18 años, devuelve la edad en Años y Meses
             {
-                $('#FechaNacimiento>label').html('('+ $Edad + ' Años,'+' '+ $DifMeses + ' ' + $PalabraMes + ')');
+                $(inputDateLabel).html('('+ $Edad + ' Años,'+' '+ $DifMeses + ' ' + $PalabraMes + ')');
             }
             else //Si es mayor de 18 años, devuelve la edad en Años
             {
-                $('#FechaNacimiento>label').html('('+ $Edad +' Años)');
+                $(inputDateLabel).html('('+ $Edad +' Años)');
             };
         }
         else
         {
             if ($Edad < 18 && $DifMeses != 0) //Si es un menor de 18 años, devuelve la edad en Años y Meses
             {
-                $('#VSFechaNacimientoLabel').html('&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;('+ $Edad + ' Años,'+' '+ $DifMeses + ' ' + $PalabraMes + ')');
+                $(inputDateLabel).html('&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;('+ $Edad + ' Años,'+' '+ $DifMeses + ' ' + $PalabraMes + ')');
             }
             else //Si es mayor de 18 años, devuelve la edad en Años
             {
-                $('#VSFechaNacimientoLabel').html('&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;('+ $Edad +' Años)');
+                $(inputDateLabel).html('&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;('+ $Edad +' Años)');
             };
         };
      };
 
-    $('#FechaNacimiento input[type="date"]').on('focusout',function(){
-            CalcularEdad('#FechaNacimiento input[type="date"]');
-    });
-
-    $('#VinculosSignificativos input[type="date"]').on('focusout',function(){
-            CalcularEdad('#VSFechaNacimiento');
-    }); 
-
-
-    
-    
-    //Inserta una nueva fila de campos para cargar otro Vinculo Significativo
+//Inserta una nueva fila de campos para cargar otro Vinculo Significativo
     var $Contador = 0;
+    var a = '';
+    var b = '';
+    
     $('#AgregarVinculo').on('click', function(){
         $Contador +=1;
         
-        var $NuevoVinculo = '<div id="VinculosSignificativos' + $Contador + '" class="col-md-6"><label class="col-md-3"> </label><label id="VSFechaNacimientoLabel' + $Contador + '" class="col-md-3 Años">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;(?? Años)</label><br><input type="text" name="VSVinculo' + $Contador + '" class="form-control col-md-1" placeholder="VÍNCULO"/><input type="text" name="VSApellidoNombre' + $Contador + '" class="form-control col-md-2" placeholder="APELLIDO Y NOMBRE"/><input type="date" name="VSFechaNacimiento' + $Contador + '" class="form-control col-md-1" id="VSFechaNacimiento" style="font-size:9px"/><input type="text" name="VSOcupacion' + $Contador + '" class="form-control col-md-1" placeholder="OCUPACIÓN"/><select name="VSConvive' + $Contador + '" class="form-control col-md-1" style="font-size:9px"><option value="Convive" default>CONVIVE</option><option value="No Convive"> NO CONVIVE</option></select></div>';
+        var $NuevoVinculo = '<div id="VinculosSignificativos' + $Contador + '" class="col-md-6"><label class="col-md-3"> </label><label id="VSFechaNacimientoLabel' + $Contador + '" class="col-md-2 Años">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;(?? Años)</label><button class="btn btn-danger QuitarVinculo"><span class="glyphicon glyphicon-remove"></span></button><br><input type="text" name="VSVinculo' + $Contador + '" class="form-control col-md-1" placeholder="VÍNCULO"/><input type="text" name="VSApellidoNombre' + $Contador + '" class="form-control col-md-2" placeholder="APELLIDO Y NOMBRE"/><input type="date" name="VSFechaNacimiento' + $Contador + '" class="form-control col-md-1" id="VSFechaNacimiento" style="font-size:9px"/><input type="text" name="VSOcupacion' + $Contador + '" class="form-control col-md-1" placeholder="OCUPACIÓN"/><select name="VSConvive' + $Contador + '" class="form-control col-md-1" style="font-size:9px"><option value="Convive" default>CONVIVE</option><option value="No Convive"> NO CONVIVE</option></select></div>';
         
         $($NuevoVinculo).insertAfter($('#VinculosSignificativos'));
     });
-        
-        
-    
+//Calcula la Edad para el (?? Años) de #FechaNacimiento
+    $('#FechaNacimiento input[type="date"]').on('focusout',function(){
+        CalcularEdad('#FechaNacimiento input[type="date"]','#FechaNacimiento>label');
+    });
 
+//Calcula la Edad para el (?? Años) de #VSFechaNacimiento 
+    $('#VSFechaNacimiento').on('focusout',function(){
+        CalcularEdad('#VSFechaNacimiento','#VSFechaNacimientoLabel');
+    }); 
 
+//Intento de que Calcule la Edad de los otros Vínculos Agregado y que elimine la file cuando se cliquea en un '.QuitarVinculo'
+    /*a = 'VSFechaNacimiento'+ $Contador; //string del Nombre del inputDate
+    b = 'VSFechaNacimientoLabel'+ $Contador; //string del Nombre del inputDateLabel
+
+    $(a).on('focusout',function(){
+    CalcularEdad(a,b);});*/
 
 
 });
-
-
-    
-
-
-
+ 
