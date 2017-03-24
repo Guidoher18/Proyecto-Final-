71210c2d8aeaed2e94015d560307cc4dee6398b6
Guidoher18 <guido_181193@outlook.com>
2017-03-24 01:55:10 -0300

    Comienzos de Adaptación con .NET

---
+++ Proyecto-Final-/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Proyecto_Final_.Startup))]
+namespace Proyecto_Final_
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
